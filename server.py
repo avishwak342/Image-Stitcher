6f5b7dc7922485590d3f9b89d7f180c6a72fc47b
avishwak342 <80733545+avishwak342@users.noreply.github.com>
2021-11-30 12:04:42 -0500

    Update server.py
    final edit

--- server.py
+++ server.py
@@ -1,46 +1,45 @@
-from flask import Flask, request, redirect, flash, render_template, send_from_directory, jsonify
+from flask import Flask, request as req, redirect, flash, render_template, send_from_directory, jsonify
 import os as o
 from flask.helpers import url_for
-from stitcher import start
+from stitcher import pStart
 from werkzeug.utils import secure_filename
-import glob as gb
 
 server = Flask(__name__, template_folder='static')
 server.secret_key = 'secret_key'
 
 # util functions
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-UPLOAD_FOLDER = o.path.join(o.getcwd(), 'images')
+ALLWD_EXTNS = set(['png', 'jpg', 'jpeg'])
+UP_FLDR = o.path.join(o.getcwd(), 'images')
 
 
-def checkFileType(filename):
-    return '.' in filename and filename.rsplit(
-        '.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def checkFileType(nameF):
+    return '.' in nameF and nameF.rsplit(
+        '.', 1)[1].lower() in ALLWD_EXTNS
 
 
 @server.route('/', methods=['GET', 'POST'])
 def home():
     # print(request.files)
     file_names = None
-    if request.method == 'POST':
-        if 'files' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        files = request.files.getlist('files')
+    if req.method == 'POST':
+        if 'files' not in req.files:
+            flash('multi part file not found')
+            return redirect(req.url)
+        files = req.files.getlist('files')
         print(files)
         file_names = []
-        for file in files:
-            print(file)
-            if file and checkFileType(file.filename):
+        for xf in files:
+            print(xf)
+            if xf and checkFileType(xf.filename):
                 print(True)
-                filename = secure_filename(file.filename)
+                filename = secure_filename(xf.filename)
                 file_names.append(filename)
                 print(filename)
-                file.save(o.path.join(UPLOAD_FOLDER, filename))
+                xf.save(o.path.join(UP_FLDR, filename))
 
-                return redirect(request.url)
+                return redirect(req.url)
 
-    if request.method == 'GET':
+    if req.method == 'GET':
         image_names = o.listdir('./images')
         print(image_names)
         return render_template("home.html", image_names=image_names)
@@ -50,21 +49,15 @@ def home():
 @server.route('/display/<filename>')
 def sendImage(filename):
     if filename == 'results':
-        return send_from_directory("results", 'final.png')
+        return send_from_directory("results", 'stitfinal.png')
     return send_from_directory("images", filename)
-
-@server.route('/process')
-def displayImages():
-    image_names = o.listdir('./images')
-    print(image_names)
-    return render_template("process.html", image_names=image_names)
 
 @server.route('/start', methods=['GET'])
 def begin():
     try:
-        process = start()
-    except Exception as e:
-        flash('error - ' + str(e) + ' - clear cache and retry')
+        pStart()
+    except Exception as xerr:
+        flash('error - ' + str(xerr) + ' - clear cache and retry')
     flash('process - completed')
     return redirect('/')
 
@@ -84,4 +77,4 @@ def cacheClear():
     return redirect('/')
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0',debug=True, port=3000)
+    server.run(threaded=True, port=3000)
